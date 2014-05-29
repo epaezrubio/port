8407d1ab914216198204cb2bec15522e1c3c9778
Eduardo Paez Rubio <epaezrubio@gmail.com>
2014-05-29 00:01:45 -0700

    main cleaned up

--- client/app/scripts/main.js
+++ client/app/scripts/main.js
@@ -5,40 +5,4 @@
     var canvas = document.getElementsByTagName('canvas')[0];
     var context = canvas.getContext('2d');
 
-    var layerSet = layeredCanvas(context, canvas);
-    
-    var l = layerSet.createLayer(4);
-    var l2 = layerSet.createLayer(3);
-    var l1 = layerSet.createLayer(1);
-    
-    
-    l.queueDraw(function (context) {
-        context.beginPath();
-        context.fillStyle = "#AAFFEE";
-        context.fillRect(15, 50, 50, 50);
-    })
-    
-    l2.queueDraw(function (context) {
-        context.beginPath();
-        context.fillStyle = "#AA00EE";
-        context.fillRect(0, 0, 50, 50);
-    })
-    
-    l2.queueDraw(function (context) {
-        context.beginPath();
-        context.fillStyle = "#AA0000";
-        context.fillRect(25, 25, 50, 50);
-    })
-    
-    l1.queueDraw(function (context) {
-        context.beginPath();
-        context.fillStyle = "#CCCCCC";
-        context.fillRect(50, 50, 50, 50);
-    })
-    
-
-    layerSet.render();
-
-
-
 }());
